0e56b8e93f7fcb5281887d765bcf549b009c2c5b
Siddiqui-2583 <nurnaby83@gmail.com>
2021-09-16 17:58:24 +0600

    Table created and data stream created from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,52 @@
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((countries) => {
+        setCountries(countries);
+        // console.log(countries);
+      })
+      .catch((e) => console.log(e.message));
+  }, []);
+  const flagStyle = {
+    height: "50px",
+  };
   return (
     <div className="App">
-      <h2>This is root</h2>
+      <table class="container">
+        <thead>
+          <tr>
+            <th>
+              <h1>Name</h1>
+            </th>
+            <th>
+              <h1>Flag</h1>
+            </th>
+            <th>
+              <h1>Population</h1>
+            </th>
+            <th>
+              <h1>Religion</h1>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {countries.map((country) => (
+            <tr>
+              <td>{country.name}</td>
+              <td>
+                <img style={flagStyle} src={country.flag} alt="" />
+              </td>
+              <td>{country.population}</td>
+              <td>{country.region}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
